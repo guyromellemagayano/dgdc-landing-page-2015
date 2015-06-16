2baefda7a46dcad5b1ad463a615602d11facf309
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2015-06-17 03:39:07 +0800

    Added compressed folder

---
+++ compressed/assets/js/lightbox/ekko-lightbox-custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function ($) {
+  var Centering = {
+    image: function(parent, modal, padded) {
+      return (($(parent).height() / 2) - ($(modal).height() / 2) - padded);
+    }
+  };
+
+  // delegate calls to data-toggle="lightbox"
+  $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({
+      always_show_close: true,
+      onContentLoaded: function() {
+        var topHeight = Centering.image($(window),$('.modal-dialog'),52);
+        $('.modal-dialog').css('top', topHeight);
+      },
+      onNavigate: function() {
+        var topHeight = Centering.image($(window),$('.modal-dialog'),52);
+        $('.modal-dialog').css('top', topHeight);
+      }
+    });
+  });
+
+});
